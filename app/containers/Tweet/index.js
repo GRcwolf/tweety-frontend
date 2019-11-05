0ea2b758e79644dab71d4d70389289d56fd5fb55
Wolf Chris <chris.wolf@bluewin.ch>
2019-11-05 08:51:52 +0100

    Added creator to tweets

--- app/containers/Tweet/index.js
+++ app/containers/Tweet/index.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Tweet = ({ content }) => (
-  <div className="tweet-wrapper col s12">
-    <div className="tweet-content">
-      <pre>{content}</pre>
-    </div>
-  </div>
-);
+const Tweet = ({ content, creator }) => {
+  const randomId = `${creator}${Date.now()}${Math.random()}`
+  return (
+    <tr className="tweet-wrapper col s12">
+      <div className="tweet-content">
+        <strong id={randomId}>{getCreator(creator, randomId)}</strong>
+        <pre>{content}</pre>
+      </div>
+    </tr>
+  )
+};
+
+function getCreator(id, randomId) {
+  const xhrRequest = new XMLHttpRequest();
+  xhrRequest.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      const { firstName, lastName } = JSON.parse(this.response);
+      document.getElementById(randomId).innerHTML = `${firstName} ${lastName}`;
+    }
+  };
+  xhrRequest.open('GET', `http://localhost:3000/getUserWithId?id=${id}`, true);
+  xhrRequest.send();
+}
 
 Tweet.propTypes = {
   content: PropTypes.string,
+  creator: PropTypes.string,
 };
 
 export default Tweet;
